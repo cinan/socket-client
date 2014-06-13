01e747fcebf350dea2386aca965b0ac4eb62705a
Cinan Rakosnik <cinan6@gmail.com>
2014-06-13 17:41:52 +0200

    Heartbeat tests aren't in group

--- test/heartbeat_tests.dart
+++ test/heartbeat_tests.dart
@@ -1,86 +1,87 @@
 part of client_tests;
 
 heartbeatTests() {
+  group('heartbeat tests', () {
+    Heart heart;
 
-  Heart heart;
+    setUp(() {
+      heart = new Heart(new Duration(milliseconds: 100));
+    });
 
-  setUp(() {
-    heart = new Heart(new Duration(milliseconds: 100));
-  });
+    tearDown(() {
+    });
 
-  tearDown(() {
-  });
+    test('heart is in calm after creation', () {
+      expect(heart.isBeating, isFalse);
+    });
 
-  test('heart is in calm after creation', () {
-    expect(heart.isBeating, isFalse);
-  });
+    test('heart is beating after heartbeat started', () {
+      heart.startBeating();
+      expect(heart.isBeating, isTrue);
+    });
 
-  test('heart is beating after heartbeat started', () {
-    heart.startBeating();
-    expect(heart.isBeating, isTrue);
-  });
+    test('heart has stopped after heartbeat stopped', () {
+      heart.startBeating();
+      heart.die();
+      expect(heart.isBeating, isFalse);
+    });
 
-  test('heart has stopped after heartbeat stopped', () {
-    heart.startBeating();
-    heart.die();
-    expect(heart.isBeating, isFalse);
-  });
+    test('after start beats are sent periodically', () {
+      int i = 0;
+      int j = 0;
 
-  test('after start beats are sent periodically', () {
-    int i = 0;
-    int j = 0;
+      heart.beatCallback = (_) => i++;
+      heart.startBeating();
 
-    heart.beatCallback = (_) => i++;
-    heart.startBeating();
+      new Timer.periodic(new Duration(milliseconds: 80), expectAsync((Timer t) {
+        heart.addResponse();
+        j++;
 
-    new Timer.periodic(new Duration(milliseconds: 80), expectAsync((Timer t) {
+        if (j == 10) {
+          t.cancel();
+        }
+      }, count: 10));
+
+      retryAsync(() => expect(i, greaterThan(8)));
+    });
+
+    test('action after response is received', () {
+      bool done = false;
+      heart.responseCallback = (_) {
+        done = true;
+      };
+
+      heart.startBeating();
       heart.addResponse();
-      j++;
 
-      if (j == 10) {
-        t.cancel();
-      }
-    }, count: 10));
+      expect(done, isTrue);
+    });
 
-    retryAsync(() => expect(i, greaterThan(8)));
-  });
+    test('death message callback is called if no pongs are received', () {
+      bool deathHappened = false;
+      int i = 0;
 
-  test('action after response is received', () {
-    bool done = false;
-    heart.responseCallback = (_) {
-      done = true;
-    };
+      heart.deathMessageCallback = () {
+        deathHappened = true;
+      };
 
-    heart.startBeating();
-    heart.addResponse();
+      heart.startBeating();
 
-    expect(done, isTrue);
-  });
+      new Timer.periodic(new Duration(milliseconds: 80), expectAsync((Timer t) {
+        heart.addResponse();
+        i++;
 
-  test('death message callback is called if no pongs are received', () {
-    bool deathHappened = false;
-    int i = 0;
+        if (i == 10) {
+          t.cancel();
 
-    heart.deathMessageCallback = () {
-      deathHappened = true;
-    };
+          expect(heart.isBeating, isTrue);
 
-    heart.startBeating();
-
-    new Timer.periodic(new Duration(milliseconds: 80), expectAsync((Timer t) {
-      heart.addResponse();
-      i++;
-
-      if (i == 10) {
-        t.cancel();
-
-        expect(heart.isBeating, isTrue);
-
-        new Timer(new Duration(milliseconds: 500), expectAsync(() {
-          expect(heart.isBeating, isFalse);
-          expect(deathHappened, isTrue);
-        }));
-      }
-    }, count: 10));
+          new Timer(new Duration(milliseconds: 500), expectAsync(() {
+            expect(heart.isBeating, isFalse);
+            expect(deathHappened, isTrue);
+          }));
+        }
+      }, count: 10));
+    });
   });
 }
